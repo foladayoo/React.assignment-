3d93b6c366c118df896999c2725390e2eaaab06a
foladayoo <155484312+foladayoo@users.noreply.github.com>
2024-11-06 12:07:18 +0100

    Add files via upload

---
+++ app.mjs
@@ -0,0 +1,10 @@
+import {sum,subtract,multiply,divide} from './maths.mjs'
+
+let sum1 = sum(3,7)
+let subtract1 = subtract(7,3)
+let multiply1 = multiply(10,5)
+let divide1 = divide(200,20)
+console.log(sum1)
+console.log(subtract1)
+console.log(multiply1)
+console.log(divide1)
---
+++ maths.mjs
@@ -0,0 +1,13 @@
+export function sum(a, b){
+    return a + b
+}
+export function subtract(a, b){
+    return a - b
+}
+export function multiply(x, y){
+    return x * y
+}
+export function divide(c, d){
+    return c / d
+}
+
